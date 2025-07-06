49d190cfc25eba3e7a38b7ad5ca3cda67f071576
Algoruu <zlfkcmspdbdp@naver.com>
2025-07-06 15:54:54 +0900

    [Feat] 프로젝트 전체에 나눔고딕 적용

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'OnCare',
       theme: ThemeData(
+        fontFamily: 'NanumBarunGothic',
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
